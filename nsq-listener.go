24e62941c10d448b1d05df2d6913877c817875a2
Luke Atherton <karandras@me.com>
2015-11-02 09:56:18 +1000

    Removed nsq client

--- nsq-listener.go
+++ nsq-listener.go
@@ -1,214 +1,214 @@
 package domain_events
 
-import (
-	"fmt"
-	"log"
-	"os"
-	"os/signal"
-	"strconv"
-	"syscall"
-	"time"
+// import (
+// 	"fmt"
+// 	"log"
+// 	"os"
+// 	"os/signal"
+// 	"strconv"
+// 	"syscall"
+// 	"time"
 
-	"github.com/bitly/go-nsq"
-	"github.com/bitly/go-simplejson"
-	"github.com/bitly/nsq/util"
-)
+// 	"github.com/bitly/go-nsq"
+// 	"github.com/bitly/go-simplejson"
+// 	"github.com/bitly/nsq/util"
+// )
 
-type NsqListener struct {
-	handler                  DomainHandler
-	appName                  string
-	topic                    string
-	nsqLookupAddressHttp     string
-	nsqNodeTcp               string
-	resultChannel            chan string
-	requireJsonValueParsed   bool
-	requireJsonValueIsNumber bool
-	requireJsonNumber        float64
-	listenStart              time.Time
-}
+// type NsqListener struct {
+// 	handler                  DomainHandler
+// 	appName                  string
+// 	topic                    string
+// 	nsqLookupAddressHttp     string
+// 	nsqNodeTcp               string
+// 	resultChannel            chan string
+// 	requireJsonValueParsed   bool
+// 	requireJsonValueIsNumber bool
+// 	requireJsonNumber        float64
+// 	listenStart              time.Time
+// }
 
-func NewMsgQListener(appName string, topic string, nsqLookupAddressHttp string, nsqNodeTcp string) *NsqListener {
+// func NewMsgQListener(appName string, topic string, nsqLookupAddressHttp string, nsqNodeTcp string) *NsqListener {
 
-	return &NsqListener{appName: appName, handler: NewHandler(), topic: topic, nsqLookupAddressHttp: nsqLookupAddressHttp, nsqNodeTcp: nsqNodeTcp}
-}
+// 	return &NsqListener{appName: appName, handler: NewHandler(), topic: topic, nsqLookupAddressHttp: nsqLookupAddressHttp, nsqNodeTcp: nsqNodeTcp}
+// }
 
-func (listener *NsqListener) shouldPassMessage(jsonMsg *simplejson.Json) (bool, bool) {
-	pass := true
-	backoff := false
+// func (listener *NsqListener) shouldPassMessage(jsonMsg *simplejson.Json) (bool, bool) {
+// 	pass := true
+// 	backoff := false
 
-	var requireJsonField = "message_type"
-	var requireJsonValue = "" //handler.correlationId
+// 	var requireJsonField = "message_type"
+// 	var requireJsonValue = "" //handler.correlationId
 
-	if requireJsonField == "" {
-		return pass, backoff
-	}
+// 	if requireJsonField == "" {
+// 		return pass, backoff
+// 	}
 
-	if requireJsonValue != "" && !listener.requireJsonValueParsed {
-		// cache conversion in case needed while filtering json
-		var err error
-		listener.requireJsonNumber, err = strconv.ParseFloat(requireJsonValue, 64)
-		listener.requireJsonValueIsNumber = (err == nil)
-		listener.requireJsonValueParsed = true
-	}
+// 	if requireJsonValue != "" && !listener.requireJsonValueParsed {
+// 		// cache conversion in case needed while filtering json
+// 		var err error
+// 		listener.requireJsonNumber, err = strconv.ParseFloat(requireJsonValue, 64)
+// 		listener.requireJsonValueIsNumber = (err == nil)
+// 		listener.requireJsonValueParsed = true
+// 	}
 
-	header, checkErr := jsonMsg.CheckGet("header")
+// 	header, checkErr := jsonMsg.CheckGet("header")
 
-	if checkErr == false {
-		log.Printf("ERROR: Unable to get header")
-		pass = false
-	}
+// 	if checkErr == false {
+// 		log.Printf("ERROR: Unable to get header")
+// 		pass = false
+// 	}
 
-	jsonVal, ok := header.CheckGet(requireJsonField)
-	if !ok {
-		pass = false
-		log.Printf("ERROR: Incorrect value: %s \n", jsonVal)
-		/*if requireJsonValue != "" {
-			log.Printf("ERROR: missing field to check required value")
-			backoff = true
-		}*/
-	} else if requireJsonValue != "" {
-		// if command-line argument can't convert to float, then it can't match a number
-		// if it can, also integers (up to 2^53 or so) can be compared as float64
-		if strVal, err := jsonVal.String(); err == nil {
-			if strVal != requireJsonValue {
-				pass = false
-			}
-		} else if listener.requireJsonValueIsNumber {
-			floatVal, err := jsonVal.Float64()
-			if err != nil || listener.requireJsonNumber != floatVal {
-				pass = false
-			}
-		} else {
-			// json value wasn't a plain string, and argument wasn't a number
-			// give up on comparisons of other types
-			pass = false
-		}
-	}
+// 	jsonVal, ok := header.CheckGet(requireJsonField)
+// 	if !ok {
+// 		pass = false
+// 		log.Printf("ERROR: Incorrect value: %s \n", jsonVal)
+// 		/*if requireJsonValue != "" {
+// 			log.Printf("ERROR: missing field to check required value")
+// 			backoff = true
+// 		}*/
+// 	} else if requireJsonValue != "" {
+// 		// if command-line argument can't convert to float, then it can't match a number
+// 		// if it can, also integers (up to 2^53 or so) can be compared as float64
+// 		if strVal, err := jsonVal.String(); err == nil {
+// 			if strVal != requireJsonValue {
+// 				pass = false
+// 			}
+// 		} else if listener.requireJsonValueIsNumber {
+// 			floatVal, err := jsonVal.Float64()
+// 			if err != nil || listener.requireJsonNumber != floatVal {
+// 				pass = false
+// 			}
+// 		} else {
+// 			// json value wasn't a plain string, and argument wasn't a number
+// 			// give up on comparisons of other types
+// 			pass = false
+// 		}
+// 	}
 
-	return pass, backoff
-}
+// 	return pass, backoff
+// }
 
-func (listener *NsqListener) HandleMessage(m *nsq.Message) error {
-	handleTime := time.Now()
+// func (listener *NsqListener) HandleMessage(m *nsq.Message) error {
+// 	handleTime := time.Now()
 
-	var err error
+// 	var err error
 
-	var requireJsonField = "header"
+// 	var requireJsonField = "header"
 
-	if requireJsonField != "" {
-		var jsonMsg *simplejson.Json
-		jsonMsg, err = simplejson.NewJson(m.Body)
-		//fmt.Printf("Got: %s\n", jsonMsg)
-		if err != nil {
-			log.Printf("ERROR: Unable to decode json: %s", m.Body)
-			return nil
-		}
+// 	if requireJsonField != "" {
+// 		var jsonMsg *simplejson.Json
+// 		jsonMsg, err = simplejson.NewJson(m.Body)
+// 		//fmt.Printf("Got: %s\n", jsonMsg)
+// 		if err != nil {
+// 			log.Printf("ERROR: Unable to decode json: %s", m.Body)
+// 			return nil
+// 		}
 
-		//check if should handle message
-		if pass, _ := listener.shouldPassMessage(jsonMsg); !pass {
-			/*if backoff {
-				return errors.New("backoff")
-			}*/
-			log.Println("ERROR: Didn't Pass")
-			return nil
-		}
+// 		//check if should handle message
+// 		if pass, _ := listener.shouldPassMessage(jsonMsg); !pass {
+// 			/*if backoff {
+// 				return errors.New("backoff")
+// 			}*/
+// 			log.Println("ERROR: Didn't Pass")
+// 			return nil
+// 		}
 
-		header, checkErr := jsonMsg.CheckGet("header")
+// 		header, checkErr := jsonMsg.CheckGet("header")
 
-		if checkErr == false {
-			log.Printf("ERROR: Unable to get header")
-			return nil
-		}
+// 		if checkErr == false {
+// 			log.Printf("ERROR: Unable to get header")
+// 			return nil
+// 		}
 
-		messageType, checkErr := header.CheckGet("message_type")
+// 		messageType, checkErr := header.CheckGet("message_type")
 
-		if checkErr == false {
-			log.Printf("ERROR: Unable to get message_type")
-			return nil
-		}
+// 		if checkErr == false {
+// 			log.Printf("ERROR: Unable to get message_type")
+// 			return nil
+// 		}
 
-		if messageTypeName, err := messageType.String(); err == nil {
+// 		if messageTypeName, err := messageType.String(); err == nil {
 
-			log.Printf("Got Message: %s", messageTypeName)
+// 			log.Printf("Got Message: %s", messageTypeName)
 
-			listener.handler.Handle(messageTypeName, m.Body)
+// 			listener.handler.Handle(messageTypeName, m.Body)
 
-			fmt.Printf("Listened for: %v\n", time.Now().Sub(listener.listenStart))
-			fmt.Printf("Handled in: %v\n", time.Now().Sub(handleTime))
-		}
-	} else {
-		log.Printf("ERROR: %s", err.Error())
-	}
+// 			fmt.Printf("Listened for: %v\n", time.Now().Sub(listener.listenStart))
+// 			fmt.Printf("Handled in: %v\n", time.Now().Sub(handleTime))
+// 		}
+// 	} else {
+// 		log.Printf("ERROR: %s", err.Error())
+// 	}
 
-	return nil
-}
+// 	return nil
+// }
 
-func (listener *NsqListener) Listen() {
+// func (listener *NsqListener) Listen() {
 
-	termChan := make(chan os.Signal, 1)
-	signal.Notify(termChan, syscall.SIGINT, syscall.SIGTERM)
+// 	termChan := make(chan os.Signal, 1)
+// 	signal.Notify(termChan, syscall.SIGINT, syscall.SIGTERM)
 
-	//resultChan := make(chan string, 1)
-	//correlationId := NewUUID().String()
-	//listenStart := time.Now()
+// 	//resultChan := make(chan string, 1)
+// 	//correlationId := NewUUID().String()
+// 	//listenStart := time.Now()
 
-	maxInFlight := 250
+// 	maxInFlight := 250
 
-	cfg := nsq.NewConfig()
-	cfg.Set("max_in_flight", maxInFlight)
-	//cfg.Set("max_backoff_duration", 10)
-	cfg.UserAgent = fmt.Sprintf("%s go-nsq/%s", listener.appName, nsq.VERSION)
+// 	cfg := nsq.NewConfig()
+// 	cfg.Set("max_in_flight", maxInFlight)
+// 	//cfg.Set("max_backoff_duration", 10)
+// 	cfg.UserAgent = fmt.Sprintf("%s go-nsq/%s", listener.appName, nsq.VERSION)
 
-	var readerOpts = util.StringArray{}
+// 	var readerOpts = util.StringArray{}
 
-	err := util.ParseOpts(cfg, readerOpts)
-	if err != nil {
-		log.Fatalf(err.Error())
-	}
+// 	err := util.ParseOpts(cfg, readerOpts)
+// 	if err != nil {
+// 		log.Fatalf(err.Error())
+// 	}
 
-	setupTime := time.Now()
+// 	setupTime := time.Now()
 
-	channel := "saas-users-app" //fmt.Sprintf("%s", h.appName)
+// 	channel := "saas-users-app" //fmt.Sprintf("%s", h.appName)
 
-	if !util.IsValidTopicName(listener.topic) {
-		log.Fatalf("--topic is invalid")
-	}
+// 	if !util.IsValidTopicName(listener.topic) {
+// 		log.Fatalf("--topic is invalid")
+// 	}
 
-	if !util.IsValidChannelName(channel) {
-		log.Fatalf("--channel is invalid")
-	}
+// 	if !util.IsValidChannelName(channel) {
+// 		log.Fatalf("--channel is invalid")
+// 	}
 
-	q, err := nsq.NewConsumer(listener.topic, channel, cfg)
-	if err != nil {
-		log.Fatalf(err.Error())
-	}
-	q.SetLogger(log.New(os.Stderr, "", log.LstdFlags), nsq.LogLevelInfo)
+// 	q, err := nsq.NewConsumer(listener.topic, channel, cfg)
+// 	if err != nil {
+// 		log.Fatalf(err.Error())
+// 	}
+// 	q.SetLogger(log.New(os.Stderr, "", log.LstdFlags), nsq.LogLevelInfo)
 
-	q.AddHandler(listener)
+// 	q.AddHandler(listener)
 
-	//for _, addrString := range nsqdTCPAddrs {
-	err = q.ConnectToNSQD(listener.nsqNodeTcp)
-	if err != nil {
-		log.Fatalf(err.Error())
-	}
-	//}
+// 	//for _, addrString := range nsqdTCPAddrs {
+// 	err = q.ConnectToNSQD(listener.nsqNodeTcp)
+// 	if err != nil {
+// 		log.Fatalf(err.Error())
+// 	}
+// 	//}
 
-	//for _, addrString := range lookupdHTTPAddrs {
-	log.Printf("lookupd addr %s", listener.nsqLookupAddressHttp)
-	err = q.ConnectToNSQLookupd(listener.nsqLookupAddressHttp)
-	if err != nil {
-		log.Fatalf(err.Error())
-	}
-	//}
+// 	//for _, addrString := range lookupdHTTPAddrs {
+// 	log.Printf("lookupd addr %s", listener.nsqLookupAddressHttp)
+// 	err = q.ConnectToNSQLookupd(listener.nsqLookupAddressHttp)
+// 	if err != nil {
+// 		log.Fatalf(err.Error())
+// 	}
+// 	//}
 
-	fmt.Printf("Setup in: %v\n", time.Now().Sub(setupTime))
+// 	fmt.Printf("Setup in: %v\n", time.Now().Sub(setupTime))
 
-	for {
-		select {
-		case <-q.StopChan:
-			return
-		case <-termChan:
-			q.Stop()
-		}
-	}
-}
+// 	for {
+// 		select {
+// 		case <-q.StopChan:
+// 			return
+// 		case <-termChan:
+// 			q.Stop()
+// 		}
+// 	}
+// }
--- nsq-publisher.go
+++ nsq-publisher.go
@@ -1,78 +1,78 @@
 package domain_events
 
-import (
-	"crypto/tls"
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"net/http"
-	"net/url"
-	"strconv"
-	"strings"
-)
+// import (
+// 	"crypto/tls"
+// 	"encoding/json"
+// 	"fmt"
+// 	"io/ioutil"
+// 	"net/http"
+// 	"net/url"
+// 	"strconv"
+// 	"strings"
+// )
 
-type nsqHttpPublisher struct {
-	nsqdHttpAddress string
-}
+// type nsqHttpPublisher struct {
+// 	nsqdHttpAddress string
+// }
 
-func NewPublisher(nsqdHttpAddress string) Publisher {
-	nsqPublisher := &nsqHttpPublisher{
-		nsqdHttpAddress: nsqdHttpAddress,
-	}
-	return nsqPublisher
-}
+// func NewPublisher(nsqdHttpAddress string) Publisher {
+// 	nsqPublisher := &nsqHttpPublisher{
+// 		nsqdHttpAddress: nsqdHttpAddress,
+// 	}
+// 	return nsqPublisher
+// }
 
-func (publisher *nsqHttpPublisher) PublishMessage(message DomainEvent) (err error) {
+// func (publisher *nsqHttpPublisher) PublishMessage(message DomainEvent) (err error) {
 
-	apiUrl := fmt.Sprintf("http://%s", publisher.nsqdHttpAddress)
-	resource := "/put"
+// 	apiUrl := fmt.Sprintf("http://%s", publisher.nsqdHttpAddress)
+// 	resource := "/put"
 
-	jsonData, _ := json.Marshal(message)
+// 	jsonData, _ := json.Marshal(message)
 
-	post_data := strings.NewReader((string)(jsonData))
+// 	post_data := strings.NewReader((string)(jsonData))
 
-	u, _ := url.ParseRequestURI(apiUrl)
-	u.Path = resource
+// 	u, _ := url.ParseRequestURI(apiUrl)
+// 	u.Path = resource
 
-	//set query params
-	q := u.Query()
-	q.Set("topic", "services")
-	u.RawQuery = q.Encode()
+// 	//set query params
+// 	q := u.Query()
+// 	q.Set("topic", "services")
+// 	u.RawQuery = q.Encode()
 
-	urlStr := fmt.Sprintf("%v", u)
+// 	urlStr := fmt.Sprintf("%v", u)
 
-	tr := &http.Transport{
-		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-	}
+// 	tr := &http.Transport{
+// 		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+// 	}
 
-	client := &http.Client{Transport: tr}
-	req, _ := http.NewRequest("POST", urlStr, post_data) // <-- URL-encoded payload
-	req.Header.Add("Content-Type", "application/json")
-	req.Header.Add("Content-Length", strconv.Itoa(post_data.Len()))
+// 	client := &http.Client{Transport: tr}
+// 	req, _ := http.NewRequest("POST", urlStr, post_data) // <-- URL-encoded payload
+// 	req.Header.Add("Content-Type", "application/json")
+// 	req.Header.Add("Content-Length", strconv.Itoa(post_data.Len()))
 
-	fmt.Println("Request to User Service: ", urlStr)
+// 	fmt.Println("Request to User Service: ", urlStr)
 
-	resp, err := client.Do(req)
+// 	resp, err := client.Do(req)
 
-	fmt.Println(resp.Status)
+// 	fmt.Println(resp.Status)
 
-	if err == nil {
-		switch resp.StatusCode {
-		case http.StatusOK:
-			return nil
-		case http.StatusCreated:
-			body, err := ioutil.ReadAll(resp.Body)
-			if err == nil {
-				fmt.Println(body)
-			} else {
-				panic(err)
-			}
-		default:
-			panic(err)
-		}
-	} else {
-		panic(err)
-	}
+// 	if err == nil {
+// 		switch resp.StatusCode {
+// 		case http.StatusOK:
+// 			return nil
+// 		case http.StatusCreated:
+// 			body, err := ioutil.ReadAll(resp.Body)
+// 			if err == nil {
+// 				fmt.Println(body)
+// 			} else {
+// 				panic(err)
+// 			}
+// 		default:
+// 			panic(err)
+// 		}
+// 	} else {
+// 		panic(err)
+// 	}
 
-	return nil
-}
+// 	return nil
+// }
